9913054f697bdc7a8eaaa72091b5f6d85873afae
bikramsk <bikramkanyal@gmail.com>
2025-06-10 10:54:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ import Registration from "./components/custom/Registration";
 import Login from "./components/custom/Login";
 import UserProfile from "./components/custom/UserProfile";
 import PrivacyPolicy from "./components/custom/PrivacyPolicy";
-import SocialFlagSong from "./components/custom/SocialFlagSong";
 import Contact from "./components/custom/Contact";
 import AboutUs from "./components/custom/AboutUs";
 import Video from "./components/custom/VideoPage";
 import { CowSeva } from "./components/cow-seva";
 import ScrollToTop from "./utils/components/ScrollToTop";
 import GotraAankna from "./components/custom/GotraAankna";
-import AllAssembly from "./components/custom/AllAssembly";
 import LatestNews from "./components/custom/LatestNews";
 import NotFound from "./components/NotFound";
 import CommunityFunds from "./components/custom/CommunityFunds";
@@ -32,12 +30,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/socialflagsong" element={<SocialFlagSong />} />
           <Route path="/contact-us" element={<Contact />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/gau-seva" element={<CowSeva />} />
           <Route path="/gotra-aankna" element={<GotraAankna />} />
-          <Route path="/all-assembly" element={<AllAssembly />} />
           <Route path="/video" element={<Video />} />
           <Route path="/latestnews" element={<LatestNews />} />
           <Route path="/community-funds" element={<CommunityFunds />} />
