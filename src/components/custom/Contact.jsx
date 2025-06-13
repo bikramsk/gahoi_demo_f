c255d12c580bdc5979f17fce285e2f943d3aad6c
bikramsk <bikramkanyal@gmail.com>
2025-06-13 16:54:52 +0530

    updated contact

--- src/components/custom/Contact.jsx
+++ src/components/custom/Contact.jsx
@@ -189,10 +189,8 @@ const Contact = () => {
           className={`bg-white rounded-lg shadow-lg p-5 md:p-8 mb-6 md:mb-10  relative ${decorativeStyles.gradientBg}`}
         >
           {/* Contact Information Section */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mb-10 md:mb-16 pt-6 md:pt-8">
-            <div
-              className={`${cardStyles} transition-all duration-300 hover:shadow-lg`}
-            >
+          <div className="grid grid-cols-1 mb-10 md:mb-16 pt-6 md:pt-8">
+            <div className={`${cardStyles} transition-all duration-300 hover:shadow-lg`}>
               <div className="flex items-center gap-4 mb-6">
                 <div className="w-10 h-10 md:w-12 md:h-12 bg-red-50 rounded-full flex items-center justify-center">
                   <svg
@@ -210,22 +208,12 @@ const Contact = () => {
                     />
                   </svg>
                 </div>
-                <h3
-                  className={`text-lg md:text-xl font-bold text-gray-800 ${languageFontClass}`}
-                >
+                <h3 className={`text-lg md:text-xl font-bold text-gray-800 ${languageFontClass}`}>
                   {t('contact.contactInfo.contact1.name')}
                 </h3>
               </div>
 
               <ul className="space-y-3">
-                {/* <li className="flex items-start">
-                  <span
-                    className={`text-gray-800 font-medium ${languageFontClass}`}
-                  >
-                    {contact1.name}
-                  </span>
-                </li> */}
-              
                 <li className="flex items-start">
                   <span className={`text-gray-700 ${languageFontClass}`}>
                     {contact1.address1}
@@ -306,101 +294,6 @@ const Contact = () => {
                   </span>
                 </li>
               </ul>
-            </div>
-
-            <div
-              className={`${cardStyles} transition-all duration-300 hover:shadow-lg`}
-            >
-              {/* <div className="flex items-center gap-4 mb-6">
-                <div className="w-10 h-10 md:w-12 md:h-12 bg-red-50 rounded-full flex items-center justify-center">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 md:h-6 md:w-6 text-red-700"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    />
-                  </svg>
-                </div>
-                <h3
-                  className={`text-lg md:text-xl font-bold text-gray-800 ${languageFontClass}`}
-                >
-                    {t('contact.contactInfo.contact2.name')}
-                </h3> 
-              </div>
-
-              <ul className="space-y-3">
-                 <li className="flex items-start">
-                  <span
-                    className={`text-gray-800 font-medium ${languageFontClass}`}
-                  >
-                    {contact2.name}
-                  </span>
-                </li> 
-                <li className="flex items-start">
-                  <span className={`text-gray-700 ${languageFontClass}`}>
-                    {contact2.address1}
-                  </span>
-                </li>
-                <li className="flex items-start">
-                  <span className={`text-gray-700 ${languageFontClass}`}>
-                    {contact2.address2}
-                  </span>
-                </li>
-              
-                <li className="flex items-start">
-                  <span className="inline-flex items-center">
-                    <svg
-                      className="h-5 w-5 text-red-700 mr-2"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                      />
-                    </svg>
-                    <a
-                      href={`tel:${contact2.phone}`}
-                      className="text-red-600 hover:text-red-700 transition-colors"
-                    >
-                      {contact2.phone}
-                    </a>
-                  </span>
-                </li>
-                <li className="flex items-start">
-                  <span className="inline-flex items-center">
-                    <svg
-                      className="h-5 w-5 text-red-700 mr-2"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                      />
-                    </svg>
-                    <a
-                      href={`mailto:${contact2.email1}`}
-                      className="text-red-600 hover:text-red-700 transition-colors"
-                    >
-                      {contact2.email1}
-                    </a>
-                  </span>
-                </li>
-              </ul> */}
             </div>
           </div>
 
